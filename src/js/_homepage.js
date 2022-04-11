7e307c67779262dc38fb3a2a649ef255d43982b1
Tulika Das <tulikadas1029@gmail.com>
2022-04-11 19:11:01 +0530

    testimonial section done

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -37,12 +37,52 @@ if (document.querySelector('.provide-better-section')) {
 }
 
 if (document.querySelector('.testimonials-section')) {
-    tns({
+    // hide all hero text
+    const heroText = document.querySelectorAll('.testimonial-text-main p')
+    heroText.forEach(text => {
+        text.style.display = 'none'
+    })
+    
+    const heroSlider = tns({
         container: '.testimonials-section .testimonials',
         items: 1,
-        nav: false,
-        controls:false,
+        nav: true,
         autoplay: true,
+        autoplayButtonOutput: false,
+        controls: false,
+    })
+    // show first hero text
+    heroText[0].style.display = 'block'
+
+    // show hero text on slide change
+    heroSlider.events.on('indexChanged', (info) => {
+        const text = heroText[info.index - 1]
+
+        if(text) {
+            heroText.forEach(text => {
+                text.style.display = 'none'
+            })
+
+            text.style.display = 'block'
+
+            // animate hero text
+            text.classList.add('animate__animated', 'animate__slideInRight')
+            text.style.setProperty('--animate-duration', '0.5s');
+            text.addEventListener('animationend', () => {
+                text.classList.remove('animate__animated', 'animate__slideInRight');
+            })
+        }
+    })
+}
+
+if (document.querySelector('.testimonial-box-sec')) {
+    tns({
+        container: '.testimonial-box-sec .slides',
+        items: 1,
+        nav: false,
+        autoplay: true,
+        controls:false,
+        autoplayTimeout:5000,
         autoplayButtonOutput: false,
     })
 }
